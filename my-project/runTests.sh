b1e780ed5079ef335b6d71cf228b3a3f0694bdb9
rosales-stephanie <43592017+rosales-stephanie@users.noreply.github.com>
2025-05-22 12:15:30 -0400

    added parens to parserExp

--- my-project/runTests.sh
+++ my-project/runTests.sh
@@ -2,7 +2,7 @@
 filepath=/Users/stephaniemerino/Downloads/writing-a-c-compiler-tests/tests/chapter_2/invalid_parse
 for file in "$filepath"/*
 do 
-    ./compiler.sh --lex $file
+    ./compiler.sh --parse $file
     printf "\n--------------------\n"
     cat $file
     printf "\n====================\n"
--- my-project/src/parser.hs
+++ my-project/src/parser.hs
@@ -16,7 +16,7 @@ expect expected given =
         _ -> if expected /= toksToTest
                  then Left $ "Error: expected \
                  \ " ++ (show $ map show expected) ++ "\
-                 \ but got " ++ (show $ map show toksToTest)
+                 \ but got " ++ (show $ map show toksToTest) ++ "\n"
              else Right restOfToks
 
 
@@ -26,82 +26,99 @@ parseId (tok : xs) =
     case tok of 
         Identifier n -> (xs, Right n)
         wrongTok -> ((tok : xs), Left $ "Error: expected Identifier but got\
-        \ " ++ (show wrongTok) ++ "\n")
+        \ " ++ (show wrongTok) ++ ";\n")
 
 
 parseInt :: [Token] -> ([Token], Either String Ast.Constant)
-parseInt [] = ([], Left $ "Error: expected Constant but got []\n")
-parseInt (tok : xs) = 
+parseInt [] = ([], Left $ "Error: expected Constant but got [];\n")
+parseInt (tok : toks) = 
     case tok of
-        Tokens.Constant n -> (xs, Right $ Ast.Constant n)
-        wrongTok -> ((tok : xs), Left $ "Error: expected Constant but got\
-        \ " ++ (show wrongTok) ++ "\n")
+        Tokens.Constant n -> (toks, Right $ Ast.Constant n)
+        wrongTok -> ((tok : toks), Left $ "Error: expected Constant but got\
+        \ " ++ (show wrongTok) ++ ";\n")
 
 
 parseExp :: [Token] -> ([Token], Either String Ast.Exp)
-parseExp toks = 
-    let (skippedInt, int) = parseInt toks
-    in case int of
-        Right c -> (skippedInt, Right $ Ast.Exp c)
-        Left err -> (toks, Left $ "Error: parseExp (int failed);\n" ++ err)
+parseExp (nextToken : toks) = 
+    case nextToken of
+        Constant _ -> 
+            let (skippedInt, consInt) = parseInt (nextToken:toks)
+            in case consInt of
+                Right c -> (skippedInt, Right $ Ast.Exp c)
+                Left err -> (toks, Left $ "Error: parseExp consInt;\n" ++ err)
+        OpenParen -> 
+            let (nextToks, innerExp) = parseExp toks
+                expCloseParen        = expect [CloseParen] nextToks 
+            in case innerExp of
+                Left err 
+                    -> (nextToks, Left $ "Error: parseExp innerExp;\n" ++ err)
+                Right exp 
+                    -> case expCloseParen of
+                           Left err -> (nextToks, Left $ "Error:\
+                                           \ parseExp expCloseParen;\n" ++ err)
+                           Right nextToks -> (nextToks, Right exp)
+        _ -> ((nextToken:toks), Left "Error: parseExp;\n")
+      -- nextToken == Tilde || nextToken == Hyphen ->
 
 
 parseStatement :: [Token] -> ([Token], Either String Ast.Statement)
 parseStatement toks =
-    let ret = expect [KeywordReturn] toks
-    in case ret of 
+    let nextToks = expect [KeywordReturn] toks
+    in case nextToks of 
         Right skippedRet -> 
             let (skippedExp, exp) = parseExp skippedRet
-                retToks           = expect [Semicolon] skippedExp
+                expSemicolon           = expect [Semicolon] skippedExp
             in case exp of
-                Right e -> case retToks of
-                               Right nextToks 
-                                   -> (nextToks, Right $ Ast.Statement $ Ast.Return e)
-                               Left err 
-                                   -> (skippedExp, Left $ "Error:\
-                                   \ parseStatement (retToks failed);\n" ++ err)
-                Left err -> (skippedExp, Left $ "Error: parseStatement\
-                                        \ (exp failed);\n" ++ err)
-        Left err -> (toks, Left $ "Error: parseStatement (ret failed);\n" ++ err) 
+                Right e -> 
+                    case expSemicolon of
+                        Right nextToks 
+                            -> (nextToks, Right $ Ast.Statement $ Ast.Return e)
+                        Left err 
+                            -> (skippedExp, Left $ "Error:\
+                                  \ parseStatement expSemicolon;\n" ++ err)
+                Left err 
+                    -> (skippedExp, Left $ "Error: parseStatement exp;\n" ++ err)
+        Left err -> (toks, Left $ "Error: parseStatement nextToks;\n" ++ err) 
     
 
 parseFunc :: [Token] -> ([Token], Either String Ast.FuncDef)
 parseFunc toks =
-    let ret = expect [KeywordInt] toks
-    in case ret of
+    let expInt = expect [KeywordInt] toks
+    in case expInt of
         Right skippedKeyWdInt -> 
             let (skippedId, id) = parseId skippedKeyWdInt
                 skippedToks     = expect [OpenParen, KeywordVoid, CloseParen, OpenBracket] skippedId
             in case id of
-                Right n -> 
+                Right name -> 
                     case skippedToks of
                         Right nextToks ->
                             let (skippedStatement, statement) = parseStatement nextToks 
-                                retToks                       = expect [CloseBracket] skippedStatement
+                                expCloseBracket               = expect [CloseBracket] skippedStatement
                             in case statement of
                                 Right state -> 
-                                    case retToks of
+                                    case expCloseBracket of
                                         Right nextToks 
-                                            -> (nextToks, Right (Ast.FuncDef {Ast.name=n, Ast.body=state}))
+                                            -> (nextToks, Right (Ast.FuncDef {Ast.name=name, Ast.body=state}))
                                         Left err 
                                             -> (skippedStatement, Left $ "Error:\
-                                                \ parseFunc (retToks failed)\
-                                                \;\n" ++ err)
+                                            \ parseFunc expCloseBracket;\n" ++ err)
                                 Left err -> (skippedStatement, Left $ "Error:\
-                                \ parseFunc (statement failed);\n" ++ err)
+                                \ parseFunc statement;\n" ++ err)
                         Left err -> (skippedId, Left $ "Error:\
-                                            \ parseFunc (skippedToks failed);\n" ++ err)
+                                            \ parseFunc skippedToks;\n" ++ err)
                 Left err -> (toks, Left $ "Error:\
-                                    \ parseFunc (id failed);\n" ++ err)
-        Left err -> (toks, Left $ "Error: parseFunc (ret failed);\n" ++ err)
+                                    \ parseFunc id;\n" ++ err)
+        Left err -> (toks, Left $ "Error: parseFunc expInt;\n" ++ err)
 
 
 parseProgram :: [Token] -> Either String Ast.Program
 parseProgram toks = 
     let (skippedMain, func) = parseFunc toks
     in case func of
-        Right f -> case skippedMain of 
-                       [] -> Right $ Ast.Program f
-                       leftovers -> Left $ "Error: parseProgram had leftovers\
-                                     \ " ++ (show leftovers)
-        Left err -> Left $ "Error: parseProgram (func failed);\n" ++ err
+        Right f -> 
+            case skippedMain of 
+                [] -> Right $ Ast.Program f
+                leftovers -> Left $ "Error: parseProgram had leftovers\
+                                                 \ " ++ (show leftovers)
+        Left err -> Left $ "Error: parseProgram\
+                            \ func;\n" ++ err ++ (show skippedMain)
