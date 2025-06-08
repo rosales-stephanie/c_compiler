2ebc8b06e0965a5a29ca98b288e75335e07e85ba
rosales-stephanie <43592017+rosales-stephanie@users.noreply.github.com>
2025-06-08 11:40:32 -0400

    pseudo to stack operand

--- my-project/src/codegen.hs
+++ my-project/src/codegen.hs
@@ -5,6 +5,43 @@ module CodeGen
 
 import qualified Tacky as T
 import Assembly
+import qualified Data.Map as Map
+
+
+--assembly -> assembly
+--returns whether the num was used or not, as well as the operand
+pseudoToStack'' :: Operand -> Map.Map String Int -> Int -> (Map.Map String Int, Operand, Int)
+pseudoToStack'' op map num = 
+    case op of 
+        Pseudo s -> 
+            let maybeNum = Map.lookup s map
+            in case maybeNum of
+                Just n  -> (map, Stack n, num)
+                Nothing -> (Map.insert s num map, Stack num, num - 4)
+        _        -> (map, op, num)
+
+
+--assembly -> assembly
+pseudoToStack :: [Instruction] -> [Instruction]
+pseudoToStack ins = 
+    let map      = Map.empty
+        stackNum = -4
+    in pseudoToStack' ins map stackNum
+
+    
+--assembly -> assembly
+pseudoToStack' :: [Instruction] -> Map.Map String Int -> Int -> [Instruction]
+pseudoToStack' [] _ _ = []
+pseudoToStack' (x : xs) map num = 
+    case x of 
+        Mov src dst      -> 
+            let (m1, opS, n1) = pseudoToStack'' src map num
+                (m2, opD, n2) = pseudoToStack'' dst m1 n1
+            in (Mov opS opD) : (pseudoToStack' xs m2 n2)
+        Ins (Unary uOp op) -> 
+            let (m1, nOp, n1) = pseudoToStack'' op map num
+            in (Ins (Unary uOp nOp)) : (pseudoToStack' xs m1 n1)
+        _           -> x : (pseudoToStack' xs map num)
 
 
 tackyToAssemblyVal :: T.Val -> Operand
@@ -45,7 +82,8 @@ tackyToAssemblyIns (x : xs) =
 convertFunc :: T.FuncDef -> FuncDef
 convertFunc func = 
     let ins = tackyToAssemblyIns $ T.ins func
-    in FuncDef {name=(T.name func), ins=ins}
+        nIn = pseudoToStack ins
+    in FuncDef {name=(T.name func), ins=nIn}
 
 
 tackyToAssembly :: T.Program -> Program
