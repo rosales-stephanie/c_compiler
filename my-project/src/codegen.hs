ff8f72cf48970269c140dfa34b10a7a6025b0277
rosales-stephanie <43592017+rosales-stephanie@users.noreply.github.com>
2025-06-13 19:09:52 -0400

    added AllocateStack fixed invalid Mov instructions

--- my-project/src/codegen.hs
+++ my-project/src/codegen.hs
@@ -8,8 +8,18 @@ import Assembly
 import qualified Data.Map as Map
 
 
+--fix invalid Mov instructions where both the source and destination are
+--Stack operands
+fixMovIns :: [Instruction] -> [Instruction]
+fixMovIns [] = []
+fixMovIns (x : xs) = 
+   case x of
+        Mov (Stack n1) (Stack n2) -> 
+            (Mov (Stack n1) (Reggie R10)) : (Mov (Reggie R10) (Stack n2)) : (fixMovIns xs)
+        _ -> x : (fixMovIns xs)
+
+
 --assembly -> assembly
---returns whether the num was used or not, as well as the operand
 pseudoToStack'' :: Operand -> Map.Map String Int -> Int -> (Map.Map String Int, Operand, Int)
 pseudoToStack'' op map num = 
     case op of 
@@ -17,31 +27,35 @@ pseudoToStack'' op map num =
             let maybeNum = Map.lookup s map
             in case maybeNum of
                 Just n  -> (map, Stack n, num)
-                Nothing -> (Map.insert s num map, Stack num, num - 4)
+                Nothing -> (Map.insert s (num - 4) map, Stack (num - 4), num - 4)
         _        -> (map, op, num)
 
 
 --assembly -> assembly
-pseudoToStack :: [Instruction] -> [Instruction]
+pseudoToStack :: [Instruction] -> ([Instruction], Int)
 pseudoToStack ins = 
     let map      = Map.empty
-        stackNum = -4
+        stackNum = 0
     in pseudoToStack' ins map stackNum
 
     
 --assembly -> assembly
-pseudoToStack' :: [Instruction] -> Map.Map String Int -> Int -> [Instruction]
-pseudoToStack' [] _ _ = []
+pseudoToStack' :: [Instruction] -> Map.Map String Int -> Int -> ([Instruction], Int)
+pseudoToStack' [] _ n = ([], n)
 pseudoToStack' (x : xs) map num = 
     case x of 
-        Mov src dst      -> 
-            let (m1, opS, n1) = pseudoToStack'' src map num
-                (m2, opD, n2) = pseudoToStack'' dst m1 n1
-            in (Mov opS opD) : (pseudoToStack' xs m2 n2)
+        Mov src dst        -> 
+            let (m1, opS, n1)  = pseudoToStack'' src map num
+                (m2, opD, n2)  = pseudoToStack'' dst m1 n1
+                (ins, sOffset) = pseudoToStack' xs m2 n2
+            in ((Mov opS opD) : ins, sOffset) 
         Ins (Unary uOp op) -> 
-            let (m1, nOp, n1) = pseudoToStack'' op map num
-            in (Ins (Unary uOp nOp)) : (pseudoToStack' xs m1 n1)
-        _           -> x : (pseudoToStack' xs map num)
+            let (m1, nOp, n1)  = pseudoToStack'' op map num
+                (ins, sOffset) = pseudoToStack' xs m1 n1
+            in ((Ins (Unary uOp nOp)) : ins, sOffset)
+        _                  -> 
+            let (ins, sOffset) = pseudoToStack' xs map num
+            in (x : ins, sOffset)
 
 
 tackyToAssemblyVal :: T.Val -> Operand
@@ -81,9 +95,11 @@ tackyToAssemblyIns (x : xs) =
 
 convertFunc :: T.FuncDef -> FuncDef
 convertFunc func = 
-    let ins = tackyToAssemblyIns $ T.ins func
-        nIn = pseudoToStack ins
-    in FuncDef {name=(T.name func), ins=nIn}
+    let ins            = tackyToAssemblyIns $ T.ins func
+        (nIn, sOffset) = pseudoToStack ins
+        addAllIn       = (AllocateStack sOffset) : nIn
+        fixedMov       = fixMovIns addAllIn
+    in FuncDef {name=(T.name func), ins=fixedMov}
 
 
 tackyToAssembly :: T.Program -> Program
